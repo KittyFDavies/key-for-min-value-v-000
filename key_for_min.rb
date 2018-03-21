f85efa4d00cde48de1608b316037e98b5f311d71
Kitty Davies <kittyfdavies@gmail.com>
2018-03-21 19:47:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,29 @@
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
   winner = ""
+  name_hash.collect do |obj, start|
+    start
+  end
+  start
+  min = start
+  if name_hash == {}
+    nil
+  else
+    name_hash.each do |obj, val|
+      if val < min
+        val = min
+        winner = obj
+      else
+        winner = nil
+      end
+    end
+    winner
+  end
+end
+
+
+def key_for_min_value(name_hash)
+  winner = ""
   name_hash.collect do |obj, val|
     val
   end
