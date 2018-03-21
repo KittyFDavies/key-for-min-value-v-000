3eae176f442c14783e37f0ae2eb826e763c19b3b
Kitty Davies <kittyfdavies@gmail.com>
2018-03-21 19:44:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,3 +18,21 @@ def key_for_min_value(name_hash)
     winner
   end
 end
+
+#def key_for_min_value(name_hash)
+#  winner = ""
+#  min = 11
+#  if name_hash == {}
+#    nil
+#  else
+#    name_hash.each do |obj, val|
+#      if val < min
+#        val = min
+#        winner = obj
+#      else
+#        winner = nil
+#      end
+#    end
+#    winner
+#  end
+#end
