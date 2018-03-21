73f53c583d0d5852e121472906b8ee5c73df47cc
Kitty Davies <kittyfdavies@gmail.com>
2018-03-21 19:48:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,28 +11,6 @@ def key_for_min_value(name_hash)
     nil
   else
     name_hash.each do |obj, val|
-      if val < min
-        val = min
-        winner = obj
-      else
-        winner = nil
-      end
-    end
-    winner
-  end
-end
-
-
-def key_for_min_value(name_hash)
-  winner = ""
-  name_hash.collect do |obj, val|
-    val
-  end
-  min = val
-  if name_hash == {}
-    nil
-  else
-    name_hash.each do |obj, val|
       if val <= min
         val = min
         winner = obj
@@ -43,21 +21,3 @@ def key_for_min_value(name_hash)
     winner
   end
 end
-
-#def key_for_min_value(name_hash)
-#  winner = ""
-#  min = 11
-#  if name_hash == {}
-#    nil
-#  else
-#    name_hash.each do |obj, val|
-#      if val < min
-#        val = min
-#        winner = obj
-#      else
-#        winner = nil
-#      end
-#    end
-#    winner
-#  end
-#end
