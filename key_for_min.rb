729e0724d35ca72753b0613184750963e114df40
Kitty Davies <kittyfdavies@gmail.com>
2018-03-21 19:57:32 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
     nil
   else
     name_hash.each do |obj, start|
-      start += min
+      min += start
     end
     min = start
     name_hash.each do |obj, val|
