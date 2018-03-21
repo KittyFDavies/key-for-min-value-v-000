0db50e4e60b50c1ec2aac2e7d25892f54c84bd42
Kitty Davies <kittyfdavies@gmail.com>
2018-03-21 19:31:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  winner = ""
+  passengers.each do |obj, val|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    else 
+      winner = false
+    end
+  end
 end
+ 
+winner 
